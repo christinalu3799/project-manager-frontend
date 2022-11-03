c9d31e33077d70432683237f5edf8beddd2cde0a
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-11-03 16:46:16 -0400

    created register page and submit btn successfully linked to App.js

--- project-manager/src/App.js
+++ project-manager/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Route, Routes} from 'react-router-dom';
 
 // import react-bootstrap components ===============================================
@@ -19,45 +19,70 @@ import DeletedProjects from './components/DeletedProjects';
 import Show from './components/Show'
 // ==================================================================================
 const App = () => {
-  return (
-    <div >
+    const [projects, setProjects] = useState([
+        {
+            project_name: 'My first project!',
+            project_deadline: '2022-11-15',
+            project_description: 'ajlfksdaj; djaksl;fd jalsk;djf a;lwkefjaw lkefjaw',
+            project_status: 'in progress',
+        },
+        {
+            project_name: 'My second project!',
+            project_deadline: '2022-12-15',
+            project_description: 'ajlfksdaj; djaksl;fd jalsk;djf a;lwkefjaw lkefjaw',
+            project_status: 'not started',
+        },
+        {
+            project_name: 'My third project!',
+            project_deadline: '2022-12-23',
+            project_description: 'ajlfksdaj; djaksl;fd jalsk;djf a;lwkefjaw lkefjaw',
+            project_status: 'not started',
+        }
+    ])
 
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand href="/index">Project Manager</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/new-project">Add New Project</Nav.Link>
-                        <NavDropdown title="See More" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="/completed-projects">View Completed Projects</NavDropdown.Item>
-                            <NavDropdown.Item href="/deleted-projects">View Deleted Projects</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="/show">show (delete later)</Nav.Link>
-                        <Nav.Link href="/register">Register</Nav.Link>
-                        <Nav.Link href="/login">Login</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+    const register = (e) => {
+        e.preventDefault()
+        console.log('register!')
+    }
+    return (
+        <div >
 
-        <Routes>
-            <Route path='/index' element={<Index />} />
-            <Route path='/new-project' element={<NewProject/>} />
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/index">Project Manager</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/new-project">Add New Project</Nav.Link>
+                            <NavDropdown title="See More" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="/completed-projects">View Completed Projects</NavDropdown.Item>
+                                <NavDropdown.Item href="/deleted-projects">View Deleted Projects</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="/show">show (delete later)</Nav.Link>
+                            <Nav.Link href="/register">Register</Nav.Link>
+                            <Nav.Link href="/login">Login</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
 
-            <Route path='/completed-projects' element={<CompletedProjects/>}/>
-            <Route path='/deleted-projects' element={<DeletedProjects/>}/>
-            
-            <Route path='/show' element={<Show/>} />
-            <Route path='/register' element={<RegisterUser/>} />
-            <Route path='/login' element={<LoginUser/>} />
+            <Routes>
+                <Route path='/index' element={<Index />} />
+                <Route path='/new-project' element={<NewProject/>} />
 
-        </Routes>
+                <Route path='/completed-projects' element={<CompletedProjects/>}/>
+                <Route path='/deleted-projects' element={<DeletedProjects/>}/>
 
-    </div>
-  );
+                <Route path='/show' element={<Show/>} />
+                <Route path='/register' element={<RegisterUser register={register}/>} />
+                <Route path='/login' element={<LoginUser/>} />
+
+            </Routes>
+
+        </div>
+    );
 }
 
 export default App;
--- project-manager/src/components/RegisterUser.js
+++ project-manager/src/components/RegisterUser.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import '../stylesheets/Register.css'
 
-const RegisterUser = () => {
+const RegisterUser = (props) => {
   return (
-    <h1>REGISTER USER PAGE</h1>
+    <div className='register'>
+        <h1>Register New User</h1>
+        <form onSubmit={props.register} className='register-form'>
+            <input type='text' id='username' placeholder='Username' className='register-input'/>
+            <input type='text' id='email' placeholder='Email' className='register-input'/>
+            <input type='text' id='password' placeholder='Password' className='register-input'/>
+            <input type='submit' value='Sign Up' className='register-submit-btn'/>
+        </form>
+    </div>
   )
 }
 
