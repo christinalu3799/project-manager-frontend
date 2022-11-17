8645288df05b71298ac0ffd96c7b042832793383
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-11-17 12:42:02 -0500

    push to redeploy frontend

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ const App = () => {
             headers: {
                 'Content-Type': 'application/json'
             }, 
-            credentials: 'same-origin'
+            credentials: 'include'
         })
         .then (res => res.json())
         .then (resJson => {
@@ -97,7 +97,7 @@ const App = () => {
             headers: {
                 'Content-Type': 'application/json'
             },
-            credentials: 'same-origin'
+            credentials: 'include'
         })
         .then (res => res.json())
         .then (resJson => {
